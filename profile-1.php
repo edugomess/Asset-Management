4a50e236b872de1c65aeef206945f10348d4b2df
David Eduardo Gomes Bicudo <58193880+edugomess@users.noreply.github.com>
2024-10-12 23:30:00 -0300

    ...

--- profile-1.php
+++ profile-1.php
@@ -41,7 +41,7 @@
                             <circle cx="17" cy="17" r="2"></circle>
                             <path d="M5 17h-2v-6l2-5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5"></path>
                         </svg></div>
-                    <div class="sidebar-brand-text mx-3"><span>Algo</span></div>
+                    <div class="sidebar-brand-text mx-3"><span>Algo </span></div>
                 </a>
                 <hr class="sidebar-divider my-0">
                 <ul class="navbar-nav text-light" id="accordionSidebar">

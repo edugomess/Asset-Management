78cfac176f4752dfbe664380b14107fdc56c89ac
David Eduardo Gomes Bicudo <58193880+edugomess@users.noreply.github.com>
2024-10-17 17:41:56 -0300

    logout

---
+++ auth.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+// Verifique se o usuário está logado
+if (!isset($_SESSION['id_usuarios'])) {
+    // Se não estiver logado, redirecione para a página de login
+    header("Location: login.php");
+    exit();
+}
+?>

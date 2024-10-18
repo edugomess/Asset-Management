7a9716fbca824bdd6e588682262ee4f28fcff989
David Eduardo Gomes Bicudo <58193880+edugomess@users.noreply.github.com>
2024-10-17 23:34:08 -0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,6 +1,6 @@
+
 <?php
 session_start();
-include 'conexao.php'; // Arquivo onde você conecta ao banco de dados
 
 // Se o usuário já está logado, redirecione-o para o dashboard
 if (isset($_SESSION['id_usuarios'])) {
@@ -8,32 +8,12 @@ if (isset($_SESSION['id_usuarios'])) {
     exit();
 }
 
-// Verifique se o formulário de login foi enviado
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Capture os dados do formulário
-    $usuario = $_POST['email'];
-    $senha = $_POST['senha'];
-
-    // Proteja contra SQL Injection e verifique as credenciais no banco de dados
-    $stmt = $conn->prepare("SELECT id FROM usuarios WHERE email = ? AND senha = ?");
-    $stmt->bind_param("ss", $usuario, $senha);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    // Verifique se o usuário existe e as credenciais estão corretas
-    if ($result->num_rows > 0) {
-        $user = $result->fetch_assoc();
-        $id_do_usuario = $user['id'];
-
-        // Se as credenciais são válidas, defina a sessão e redirecione
-        $_SESSION['id_usuarios'] = $id_do_usuario; // Armazene o ID do usuário na sessão
-        header("Location: dashboard.php"); // Redirecione para a página principal
-        exit();
-    } else {
-        echo "Login ou senha inválidos";
-    }
-
-    $stmt->close();
-    $conn->close();
+// Código de autenticação do login
+if ($login_valido) {
+    $_SESSION['id_usuarios'] = $id_do_usuario; // Armazene o ID do usuário na sessão
+    header("Location: dashboard.php"); // Redirecione para a página principal
+    exit();
+} else {
+    echo "Login ou senha inválidos";
 }
 ?>

60d6c7b1780c3b8e73905e89189b5d6d6509c05f
David Eduardo Gomes Bicudo <58193880+edugomess@users.noreply.github.com>
2024-11-20 22:19:49 -0300

    Update inserir_usuario.php

--- inserir_usuario.php
+++ inserir_usuario.php
@@ -16,7 +16,6 @@ $confirmarSenha = $_POST['confirmarSenha'];
 $nivelUsuario = $_POST['nivelUsuario'];
 $unidade = $_POST['unidade'];
 $status = $_POST['status'];
-
 // Verifica se as senhas coincidem
 if ($senha !== $confirmarSenha) {
     echo "<script>alert('As senhas não coincidem.');</script>";

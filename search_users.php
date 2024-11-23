48da25296f677ce21a3fc2e2992f65d4d3fdb21f
David Eduardo Gomes Bicudo <58193880+edugomess@users.noreply.github.com>
2024-11-22 22:21:53 -0300

    Atribuição

--- search_users.php
+++ search_users.php
@@ -9,14 +9,14 @@ $query = isset($_GET['query']) ? mysqli_real_escape_string($conn, $_GET['query']
 // Verifique se há uma entrada de pesquisa
 if (!empty($query)) {
     // Consulte os usuários no banco de dados
-    $sql = "SELECT id, nome FROM usuarios WHERE nome LIKE '%$query%' LIMIT 10";
+    $sql = "SELECT id_usuarios, nome FROM usuarios WHERE nome LIKE '%$query%' LIMIT 10";
     $result = mysqli_query($conn, $sql);
 
     $users = [];
     if ($result) {
         while ($row = mysqli_fetch_assoc($result)) {
             $users[] = [
-                'id' => $row['id'],
+                'id' => $row['id_usuarios'],
                 'name' => $row['nome']
             ];
         }

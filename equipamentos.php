189391061c09080b55de757c524818f192c6b6bc
David Eduardo Gomes Bicudo <58193880+edugomess@users.noreply.github.com>
2024-11-24 03:51:00 -0300

    Update equipamentos.php

--- equipamentos.php
+++ equipamentos.php
@@ -515,7 +515,38 @@ function assignUser(userId, userName) {
 
 </script>
 
+<div class="pagination justify-content-start">
+    <nav>
+    <ul class="pagination">
+    <?php
+    // Previous Page Link
+    if ($current_page > 1) {
+        echo "<li class='page-item'><a class='btn btn-dark' href='?page=" . ($current_page - 1) . "'>« Anterior</a></li>";
+    }
 
+    // Page Links
+    for ($page = 1; $page <= $total_pages; $page++) {
+        if ($page == $current_page) {
+            echo "<li class='page-item active'><a class='btn btn-dark' href='?page=$page'>$page</a></li>"; // Current page
+        } else {
+            echo "<li class='page-item'><a class='btn btn-dark' href='?page=$page'>$page</a></li>"; // Other pages
+        }
+    }
+
+    // Next Page Link
+    if ($current_page < $total_pages) {
+        echo "<li class='page-item'><a class='btn btn-dark' href='?page=" . ($current_page + 1) . "'>Próximo »</a></li>";
+    }
+    ?>
+</ul>
+
+    </nav>
+</div>
+
+
+<?php
+mysqli_close($conn);
+?>
 </footer>
 
         </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
